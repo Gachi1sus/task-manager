dbf64a4a675d988715cf29569fa4355bf9c2e82b
Gachi_sus <gachisus69@gmail.com>
2026-06-26 16:13:08 +0500

    add DRF

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import tasks, task, addtask, updatetask, deletetask
+from .api_views import api_tasks, api_task
 urlpatterns = [
     path('', tasks, name='tasks'),
     path('task/<taskid>/', task, name='task'),
     path('addtask/', addtask, name='addtask'),
     path('updatetask/<taskid>/', updatetask, name='updatetask'),
     path('deletetask/<taskid>/', deletetask, name='deletetask'),
+    path('api/tasks/', api_tasks, name='api_tasks'),
+    path('api/tasks/<taskid>', api_task, name='api_task'),
+
 ]
